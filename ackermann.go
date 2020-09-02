4a409e8e283631f5d5cf1987ca574d31b129f869
Thomas Bley <mails@thomasbley.de>
2020-09-02 19:24:46 +0200

    added ackermann.go and ackermann.php to demonstrate go integration into php using php-ffi

---
+++ ackermann.go
@@ -0,0 +1,44 @@
+/**
+ * Example library to provide a go implementation of the recursive ackermann function.
+ * This library can be loaded in PHP using PHP-FFI (https://www.php.net/manual/en/book.ffi.php).
+ *
+ * 2 variants of the function are provided:
+ *
+ * ackermann(n, m) using integers as input and output
+ * ackermann_json using json strings as input and output
+ *
+ * examples:
+ *   println(ackermann(3, 11));
+ *   println(C.GoString(ackermann_json(C.CString("[3, 11]"))));
+ *
+ * compiling: go build -o ackermann.so -buildmode=c-shared ackermann.go
+ */
+package main
+
+import "C"
+import "encoding/json"
+
+func main() {
+}
+
+//export ackermann
+func ackermann(n int, m int) int {
+	if n == 0 {
+		return m + 1
+	} else if m == 0 {
+		return ackermann(n-1, 1)
+	}
+	return ackermann(n-1, ackermann(n, m-1))
+}
+
+//export ackermann_json
+func ackermann_json(input *C.char) *C.char {
+	var params [2]int
+	json.Unmarshal([]byte(C.GoString(input)), &params)
+
+	result := ackermann(params[0], params[1])
+
+	data, _ := json.Marshal(result)
+
+	return C.CString(string(data))
+}
---
+++ ackermann.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * This script integrates Go code into PHP.
+ *
+ * Go code is compiled as a shared object file (.so) and loaded in PHP using PHP-FFI.
+ * (https://www.php.net/manual/en/book.ffi.php)
+ *
+ * Performance of PHP and Go is compared using implementations
+ * of the recursive ackermann function (https://en.wikipedia.org/wiki/Ackermann_function).
+ *
+ * PHP-FFI can automatically convert basic Go types (long, char*) into PHP types.
+ * For using more complex data structures, I'm also providing an example using JSON strings as input and output.
+ *
+ * Usage:
+ * go build -o ackermann.so -buildmode=c-shared ackermann.go
+ * php ackermann.php
+ *
+ * Example output:
+ * PHP
+ * - result: 16381
+ * - time: 13.5768s
+ * Go
+ * - result: 16381
+ * - time: 0.4876s
+ * Go (using json as input/output)
+ * - result: 16381
+ * - time: 0.4840s
+ *
+ * In this example Go 1.14 is 27 times faster than PHP 7.4.
+ *
+ * Go is a statically typed, compiled programming language that offers more performance and concurrency.
+ * Combining PHP and Go gives a lot of new opportunities.
+ *
+ * Writing and integrating PHP extensions based on C/C++ can be difficult and time consuming.
+ * This can be made easier and more flexible using Go and PHP-FFI.
+ */
+
+error_reporting(E_ALL);
+
+function ackermann(int $n, int $m): int {
+    if ($n == 0) {
+        return $m + 1;
+    } else if ($m == 0) {
+        return ackermann($n - 1, 1);
+    } else {
+        return ackermann($n - 1, ackermann($n, $m - 1));
+    }
+}
+
+$start = microtime(true);
+echo 'PHP' . PHP_EOL;
+echo '- result: ' . ackermann(3, 11) . PHP_EOL;
+echo '- time: ' . number_format(microtime(true) - $start, 4) . 's' . PHP_EOL;
+
+
+$ffi = FFI::cdef('long ackermann(long p0, long p1);', __DIR__ . '/ackermann.so');
+
+$start = microtime(true);
+echo 'Go' . PHP_EOL;
+echo '- result: ' . $ffi->ackermann(3, 11) . PHP_EOL;
+echo '- time: ' . number_format(microtime(true) - $start, 4) . 's' . PHP_EOL;
+
+
+$ffi = FFI::cdef('char* ackermann_json(char* p0);', __DIR__ . '/ackermann.so');
+
+$start = microtime(true);
+$result = $ffi->ackermann_json(json_encode([3, 11]));
+echo 'Go (using json as input/output)' . PHP_EOL;
+echo '- result: ' . json_decode(FFI::string($result)) . PHP_EOL;
+echo '- time: ' . number_format(microtime(true) - $start, 4) . 's' . PHP_EOL;
